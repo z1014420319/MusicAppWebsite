c8f622e78cbccf5c60b3b855ab59da9797cde494
HU <1014420319@qq.com>
2019-10-29 01:18:35 +0800

    fixstorage

---
+++ precache-manifest.bc908ad40012d11f52f7f95f6a166f4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09b87bfd53cf19cfcf92dc1b9d4f03c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9273b25791671d2b277c",
+    "url": "/static/css/main.9c611763.chunk.css"
+  },
+  {
+    "revision": "e67398acffc444788fec",
+    "url": "/static/js/2.0d3a7997.chunk.js"
+  },
+  {
+    "revision": "9273b25791671d2b277c",
+    "url": "/static/js/main.564dde35.chunk.js"
+  },
+  {
+    "revision": "96bdcab12386c1b9e808",
+    "url": "/static/js/runtime~main.710acf0f.js"
+  }
+]);
